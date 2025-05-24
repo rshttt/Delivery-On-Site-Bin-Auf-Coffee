0fcb75c6253d3f51a6bf087f6c8f77b8dddb5874
rshttt <166872097+rshttt@users.noreply.github.com>
2025-05-25 00:18:16 +0700

    Update LoginActivity.kt

--- src/main/java/com/example/deliveryonsitebinaufcoffee/LoginActivity.kt
+++ src/main/java/com/example/deliveryonsitebinaufcoffee/LoginActivity.kt
@@ -10,18 +10,34 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,18 +48,23 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.deliveryonsitebinaufcoffee.ui.theme.AppTheme
 import kotlinx.coroutines.delay
+import kotlinx.serialization.Serializable
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -112,23 +133,49 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 fun LoginPage(modifier: Modifier = Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
+    var username by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var signUp by rememberSaveable { mutableStateOf(true) }
+    val controller = rememberNavController()
 
     Login(
         email = email,
+        signUp = signUp,
+        username = username,
         password = password,
+        onClickSignUp = {
+            signUp = true
+            controller.navigate(SignIn)
+        },
+        onClickSignIn = {
+            signUp = false
+            controller.navigate(SignUp)
+        },
         inputEmail = { newEmail -> email = newEmail },
-        inputPassword = { newPassword -> password = newPassword }
+        inputUsername = { newUsername -> username = newUsername },
+        inputPassword = { newPassword -> password = newPassword },
+        navigation = controller
     )
 }
+
+@Serializable
+object SignUp
+@Serializable
+object SignIn
 
 @Composable
 fun Login(
     modifier: Modifier = Modifier,
+    signUp: Boolean,
     email: String,
+    username: String,
     password: String,
+    onClickSignUp: () -> Unit,
+    onClickSignIn: () -> Unit,
     inputEmail: (String) -> Unit,
-    inputPassword: (String) -> Unit
+    inputUsername: (String) -> Unit,
+    inputPassword: (String) -> Unit,
+    navigation: NavHostController
 ) {
     Box (
         modifier
@@ -152,6 +199,87 @@ fun Login(
                 fontSize = 32.sp,
                 fontFamily = FontFamily(Font(resId = R.font.lemon_reg))
             )
+
+            Spacer(modifier.height(40.dp))
+
+            Row {
+                Button(
+                    onClick = onClickSignUp,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = if(!signUp) MaterialTheme.colorScheme.tertiary.copy(0.3f) else MaterialTheme.colorScheme.tertiary
+                    ),
+                    shape = RectangleShape,
+                    elevation = null,
+                    contentPadding = PaddingValues(0.dp)
+                ) {
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Sign Up",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(resId = R.font.lexend_light)),
+                            modifier = Modifier
+                                .padding(bottom = 12.dp)
+                        )
+
+                        HorizontalDivider(
+                            modifier.width(124.dp),
+                            thickness = 1.dp,
+                            color = LocalContentColor.current
+                        )
+                    }
+                }
+
+                Button(
+                    onClick = onClickSignIn,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = if(signUp) MaterialTheme.colorScheme.tertiary.copy(0.3f) else MaterialTheme.colorScheme.tertiary
+                    ),
+                    shape = RectangleShape,
+                    elevation = null,
+                    contentPadding = PaddingValues(0.dp)
+                ) {
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Sign In",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(resId = R.font.lexend_light)),
+                            modifier = Modifier
+                                .padding(bottom = 12.dp)
+                        )
+
+                        HorizontalDivider(
+                            modifier.width(124.dp),
+                            thickness = 1.dp,
+                            color = LocalContentColor.current
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier.height(20.dp))
+
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                NavHost(
+                    navController = navigation,
+                    startDestination = SignUp,
+                    modifier = modifier
+                ) {
+                    composable<SignUp> {
+                        SignUpMenu(email, username, password, inputEmail, inputUsername, inputPassword)
+                    }
+                    composable<SignIn> {
+                        SignInMenu(email, password, inputEmail, inputPassword)
+                    }
+                }
+            }
         }
 
         Box(
@@ -192,13 +320,165 @@ fun Login(
     }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting()
+fun SignUpMenu(
+    email: String,
+    username: String,
+    password: String,
+    inputEmail: (String) -> Unit,
+    inputUsername: (String) -> Unit,
+    inputPassword: (String) -> Unit
+) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    Column (
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = inputEmail,
+            placeholder = {
+                Text(
+                    text = "Email",
+                    color = Color(0xFF735557).copy(alpha = 0.3f)
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.outline,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+
+                ),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(268.dp)
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = inputUsername,
+            placeholder = {
+                Text(
+                    text = "Username",
+                    color = Color(0xFF735557).copy(alpha = 0.3f)
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.outline,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+
+                ),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(268.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = inputPassword,
+            placeholder = {
+                Text(
+                    text = "Password",
+                    color = Color(0xFF735557).copy(alpha = 0.3f)
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.outline,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+
+                ),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { passwordVisible = !passwordVisible },
+                    modifier = Modifier
+                        .size(20.dp)
+                ) {
+                    Icon(
+                        imageVector = if(passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                        contentDescription = null
+                    )
+                }
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(268.dp)
+        )
     }
 }
+
+@Composable
+fun SignInMenu(
+    email: String,
+    password: String,
+    inputEmail: (String) -> Unit,
+    inputPassword: (String) -> Unit
+) {
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    Column (
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = inputEmail,
+            placeholder = {
+                Text(
+                    text = "Email",
+                    color = Color(0xFF735557).copy(alpha = 0.3f)
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.outline,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+
+                ),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(268.dp)
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = inputPassword,
+            placeholder = {
+                Text(
+                    text = "Password",
+                    color = Color(0xFF735557).copy(alpha = 0.3f)
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = MaterialTheme.colorScheme.outline,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+
+                ),
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(
+                    onClick = { passwordVisible = !passwordVisible },
+                    modifier = Modifier
+                        .size(20.dp)
+                ) {
+                    Icon(
+                        imageVector = if(passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                        contentDescription = null
+                    )
+                }
+            },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .width(268.dp)
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AppTheme {
+//        Greeting()
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
