dc6237021ef71fccbd38585f3c7727c51157432f
faisalganteng1265 <faisalakmal2105@gmail.com>
2025-06-18 02:13:56 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
+// File: /settings.gradle.kts (Folder paling luar proyek Anda)
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+// Terapkan plugin 'foojay-resolver' untuk menangani download JDK secara otomatis
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Delivery On Site Bin Auf Coffee"
+include(":app")
