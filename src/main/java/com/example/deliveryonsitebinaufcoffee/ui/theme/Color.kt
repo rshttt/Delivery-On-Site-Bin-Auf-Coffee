7105f8dd5e886b135a3b3a4c094f7d8d03d17cdf
rshttt <166872097+rshttt@users.noreply.github.com>
2025-07-01 11:43:30 +0700

    Delete src directory

--- src/main/java/com/example/deliveryonsitebinaufcoffee/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.deliveryonsitebinaufcoffee.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val BaseColor1 = Color(0xFFDBDBDB)
-val BaseColor2 = Color(0xFFC9B194)
-val BaseColor3 = Color(0xFFA08963)
-val BaseColor4 = Color(0xFF706D54)
