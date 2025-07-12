54da1de32efb89214617469090d930de31e11e78
rshttt <166872097+rshttt@users.noreply.github.com>
2025-07-13 00:08:00 +0700

    fix bug for mobile app API

--- project-web/routes/api.php
+++ project-web/routes/api.php
@@ -22,7 +22,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
-    
+
+    Route::post('/invoices', [InvoiceController::class, 'store']);
     Route::get('/invoices/{code}', [InvoiceController::class, 'show']);
     Route::put('/invoices/{code}', [InvoiceController::class, 'update']);
     
